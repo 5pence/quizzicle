d8ac9356fe7a82fe02c8dea7e1610c146df6cff7
spencer barriball <sbarriball@me.com>
2022-02-03 14:55:27 +0000

    add api fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from "react";
+
 function App() {
+
+  const [questions, setQuestions] = React.useState()
+  let [quizReady, setQuizReady] = React.useState(false)
+
+  function getQuestions() {
+    const qs = fetch('https://opentdb.com/api.php?amount=5')
+      .then(res => res.json())
+      .then(response => {
+        setQuestions(response.results)
+        console.log(response.results);
+        setQuizReady(true)
+      })
+  }
+
+
   return (
     <>
       <div className="container">
         <h1>Quizzicle</h1>
-        <button className="start-btn">Start Quiz</button>
+        <button className="start-btn" onClick={getQuestions}>Start Quiz</button>
       </div>
     </>
   );
